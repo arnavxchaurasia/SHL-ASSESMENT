f3b48916572f90d6b43268c1df7e59476fef1af3
arnavxchaurasia <arnavxchaurasia@gmail.com>
2026-05-16 23:01:35 +0530

    Clean SHL deployment structure

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 
+# Must be set BEFORE any other imports
 os.environ["TRANSFORMERS_CACHE"] = "/tmp/hf_cache"
 os.environ["HF_HOME"] = "/tmp/hf_home"
+os.environ["SENTENCE_TRANSFORMERS_HOME"] = "/tmp/st_cache"
+os.environ["HF_DATASETS_CACHE"] = "/tmp/datasets_cache"
 
 from fastapi import FastAPI
-
 from app.api.routes import router
 
 app = FastAPI(
